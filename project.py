a0410fb9838586737558bfc3032f9bb886b925d5
Maizono-san <hungchap2@gmai.com>
2022-10-09 23:54:37 +0700

    FirstCommit

---
+++ project.py
@@ -0,0 +1,99 @@
+from tkinter import *
+
+window = Tk()
+
+window.geometry('950x550')
+window.title("Weather Forecast")
+icon = PhotoImage(file = 'icon.png')
+window.iconphoto(True,icon)
+window.config(background = '#26426a')
+window.resizable(False,False)
+
+#BackGroundImage
+background = PhotoImage(file = 'background.png')
+background_image = Label(window,
+                         image = background)
+background_image.pack()
+
+#SearchBar
+searchbar = PhotoImage(file = 'searchbar.png')
+Label (window,
+       image = searchbar,
+       bg = '#4d99e7'
+       ).place(x = 245,y = 4)
+
+#HomeButton
+home = PhotoImage(file = 'home.png')
+Button (window,
+        image = home,
+        bg = '#4d99e7',
+        activebackground = '#4d99e7',
+        cursor = 'hand2',
+        ).place(x = 5,y = 5)
+
+
+#SettingButton
+setting = PhotoImage(file = 'setting.png')   
+Button (window,
+        image = setting,
+        bg = '#4d99e7',
+        activebackground = '#4d99e7',
+        cursor = 'hand2'
+        ).place(x = 899,y = 5)
+
+#SearchButton
+search = PhotoImage(file = 'search.png')
+Button (window,
+        image = search,
+        bg = '#69adf4',
+        activebackground= '#69adf4',
+        borderwidth = 0,
+        cursor = 'hand2'
+        ).place(x = 595,y = 11)
+
+#Entry
+Entry  (window,
+        width = 18,
+        font = ('poppins',20,'bold'),
+        fg = 'white',
+        insertbackground = 'white',
+        bg = '#69adf4',
+        border = 0
+        ).place(x = 317,y = 12)
+
+#BigBox
+bigbox = PhotoImage(file = 'big.png')
+bigbox_image = Label(window,
+                     image = bigbox,
+                     bg = '#4d99e7',
+                     relief = GROOVE)
+bigbox_image.place(x = 20,y = 390)
+
+#SmallBoxes
+smallbox = PhotoImage(file = 'small.png')
+Label(window,
+      image = smallbox,
+      bg = '#4d99e7',
+      relief = GROOVE).place(x = 300,y = 405)
+Label(window,
+      image = smallbox,
+      bg = '#4d99e7',
+      relief = GROOVE).place(x = 410 ,y = 405)
+Label(window,
+      image = smallbox,
+      bg = '#4d99e7',
+      relief = GROOVE).place(x = 520 ,y = 405)
+Label(window,
+      image = smallbox,
+      bg = '#4d99e7',
+      relief = GROOVE).place(x = 630 ,y = 405)
+Label(window,
+      image = smallbox,
+      bg = '#4d99e7',
+      relief = GROOVE).place(x = 740 ,y = 405)
+Label(window,
+      image = smallbox,
+      bg = '#4d99e7',
+      relief = GROOVE).place(x = 850 ,y = 405)
+
+window.mainloop()
