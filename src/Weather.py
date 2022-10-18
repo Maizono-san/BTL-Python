107c2c0384b8f3c27f06fd9ba5bf1fbcb9745992
Stargazer131 <naotomori131@gmail.com>
2022-10-18 17:33:27 +0700

    add get city

--- src/Weather.py
+++ src/Weather.py
@@ -1,17 +1,17 @@
-from requests import get
+from pprint import pprint
+import requests
 
 
 WEATHER_KEY = '922fa7db5e80194935a053d2bbab7354'
 IP_KEY = '854633293d1654'
 
-weather_data = {}
 
 def get_current_ip() -> str:
     """
     Returns the current IP
     """
     url = 'https://api.ipify.org'
-    return get(url).content.decode('utf-8')
+    return requests.get(url).content.decode('utf-8')
 
 
 def get_current_location() -> tuple[float, float]:
@@ -19,19 +19,33 @@ def get_current_location() -> tuple[float, float]:
     Returns the current location in latitude and longitude
     """
     url = f'https://ipinfo.io/{get_current_ip()}?token={IP_KEY}'
-    data = get(url).json()
+    data = requests.get(url).json()
     latitude, longitude = [float(x) for x in data['loc'].split(',')]
     return (latitude, longitude)
 
 
 def get_weather(latitude: float, longitude: float) -> dict:
+    """
+    Returns data of the weather from the given latitude and longitude
+    """
     url = f'https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&units=metric&exclude=hourly,minutely&appid={WEATHER_KEY}'
-    
-    
+    return requests.get(url).json()
+
+
+def get_city(city: str) -> tuple[float, float]:
+    """
+    Returns the current latitude and longitude of the given city
+    """
+    url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid={WEATHER_KEY}'
+    data = requests.get(url).json()
+    pprint(data[0])
+    latitude, longitude = data[0]['lat'], data[0]['lon']
+    return (latitude, longitude)
+
 
 def main():
-    print(get_current_location())
-
+    get_city(input('City: '))
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
